ffc20b53dd6f163f865c5e7d61f6d6d4d3a555c7
grdipper <grdipper@ncsu.edu>
2016-02-27 14:43:25 -0500

    Added files via upload

--- BusinessAccumulator.java
+++ BusinessAccumulator.java
@@ -1,9 +1,19 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.text.DecimalFormat;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JTextField;
 
-public class BusinessAccumulator implements ActionListener {
+
+public class BusinessAccumulator implements ActionListener, KeyListener {
 
 	// globalUserInputValueMath is the User's input string in correct double
 	// form
@@ -11,16 +21,51 @@ public class BusinessAccumulator implements ActionListener {
 	static double accumulatedValue = 0;
 	
 	//Grant's GUI variables
+	JFrame	accWindow	= new JFrame();
+	JButton accButton	= new JButton();
+	JButton clearButton	= new JButton("Clear");
+	JTextField inNumber	= new JTextField(10); //sets the length of the text field
+	JTextField tot		= new JTextField(5); // sets the length of the text field
+	JTextField logArea	= new JTextField();
+	JTextField errorMsg = new JTextField();
+	JScrollBar scroll	= new JScrollBar();
+	JPanel clrtotnum	= new JPanel();
+	JLabel totLabel		= new JLabel("Total ="); //Text right beside the "tot" text field
+	JLabel inNumLabel	= new JLabel("Enter amount to add =>"); // Text right beside the "inNumber" text field 
+	
+	
 	
 	
 	
 	public BusinessAccumulator() {
-		// TODO Auto-generated constructor stub
-		
 		//Build the GUI
+		accWindow.setSize(700,300);
+		accWindow.setLocation(250, 200);
+		accWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		accWindow.getContentPane().add(accButton,"Center");
+		accWindow.getContentPane().add(logArea,"Center");
+		accWindow.getContentPane().add(errorMsg,"South");
+		accWindow.getContentPane().add(clrtotnum,"North");
+		
+		accWindow.setTitle("Business Accumulator - Enter an amount to be added (minus sign OK)");
+		accButton.setMnemonic(KeyEvent.VK_ENTER);
+		
+		clrtotnum.add(clearButton);
+		clrtotnum.add(totLabel);
+		clrtotnum.add(tot);
+		clrtotnum.add(inNumLabel);
+		clrtotnum.add(inNumber);
+		
+		tot.setText("$0");
+		tot.setBackground(Color.CYAN);
 		
 		
 		
+		accWindow.setVisible(true);
+		
+		
+		accButton.addActionListener(this);
+		clearButton.addActionListener(this);
 		
 		
 		
@@ -34,12 +79,11 @@ public class BusinessAccumulator implements ActionListener {
 
 	
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if(e.getSource()==ACCUMULATEBUTTON) {
-			
+		if(e.getSource()==accButton) {
+			System.out.println("Acc Button Pressed");
 			//CHECK FOR ERRORS IN THE textArea
 			
-			if(CheckForUserInput(textArea.getText())) {
+			if(CheckForUserInput(inNumber.getText())) {
 			
 				//No errors in textArea
 				//Continue on to accumulate the value and displaying it in the "log" textArea
@@ -60,11 +104,10 @@ public class BusinessAccumulator implements ActionListener {
 		
 		}
 		
-		if(e.getSource()==CLEARBUTTON) {
-			// Code for clear button
-		
-		
-		
+		if(e.getSource()==clearButton) {
+			tot.setText("$0");
+			inNumber.setText("");
+	
 		}
 	
 	}
@@ -88,95 +131,19 @@ public class BusinessAccumulator implements ActionListener {
 	
 	***********************************************************************************
 	*/
-	private static boolean CheckForUserInput(String userInput) {
-		boolean userInputContains_d = false;
-		boolean userInputContains_$ = false;
-		// for InputContainsTwoAfterDecimal, 0 indicates no decimal present, 1
-		// indicates decimal present and
-		// satisfies parameters, 2 indicates that there is a decimal present and
-		// it follows parameters
-		int userInputContainsTwoAfterDecimal = 0;
-		boolean userInputContainsDoubleParseError = false;
-		double userValue = 0;
-		String storedUserInput = userInput;
-
-		// Remove excess whitespace and handles a blank input
-		userInput = userInput.trim();
-
-		if (userInput == "") {
-			errorWithUserInput = "";
-			globalUserInputValueMath = 0;
-			return false;
-		}
-
-		// Removes d from the string and sets the boolean value to true
-		if (userInput.endsWith("d")) {
-			if (userInput.length() > 0 && userInput.charAt(userInput.length() - 1) == 'd') {
-				userInput = userInput.substring(0, userInput.length() - 1);
-				userInputContains_d = true;
-			}
-
-		}
-		// removes $ from the string and sets the boolean value to true
-		if (userInput.startsWith("$")) {
-			if (userInput.length() > 0 && userInput.charAt(0) == '$') {
-				userInput = userInput.substring(1, userInput.length());
-				userInputContains_$ = true;
-			}
-
-		}
-
-		if (userInput.contains(".")) {
-			userInputContainsTwoAfterDecimal = 1;
-			int decimalLocation = userInput.indexOf('.');
-			if ((userInput.length() - 3) <= decimalLocation) {
-				userInputContainsTwoAfterDecimal = 2;
-				// //Trace Statements
-				// System.out.println(userInput.length());
-				// System.out.println(decimalLocation);
-				// System.out.println("truee");
-			}
-
-		}
-
-		// Tests the string for everything else
-		try {
-			userValue = Double.parseDouble(userInput);
-		} catch (IllegalArgumentException ex) {
-
-			userInputContainsDoubleParseError = true;
-		}
-
-		globalUserInputValueMath = 0;
-
-		if (userInputContainsDoubleParseError == true) {
-			errorWithUserInput = "amount is not numeric";
-			return false;
-
-		} else if (userInputContainsTwoAfterDecimal == 1) {
-			errorWithUserInput = "Two decimal digits are required following a decimal point.";
-			return false;
-		} else {
-			// CheckUserInput method changes the global variable to equal it's
-			// math form instead of a string
-			globalUserInputValueMath = userValue;
-			return true;
-
-		}
 	
 	void AccumulateTotal() {
 		//Local string variable to be added to LOGAREA
 		
 		String newAccumulate = "";
-		double oldAccumulate=accumulatedValue;
+		
 		if(globalUserInputValueMath>=0) {
 			//POSITIVE NUMBER TYPED
 			//Shows up in "log" textArea as "old_value + typed_value = new_value"
 			//Check Bowman's program if unsure of "log" format
 			
 			//Set string newAccumulate to what it should look like here
-			accumulatedValue =globalUserInputValueMath + accumulatedValue;
-			newAccumulate= oldAccumulate + " + " + globalUserInputValueMath + " = " + accumulatedValue; 
+			
 			
 		}
 		
@@ -186,9 +153,7 @@ public class BusinessAccumulator implements ActionListener {
 			//Check Bowman's program if unsure of "log" format
 			
 			//Set string newAccumulate to what it should look like here
-			double absolute_userinput=abs(globalUserInputValueMath);//takes absolute value of our globalUserInputValueMath
-			accumulatedValue = globalUserInputValueMath + accumulatedValue;//calculates new  accumulated Value 
-			newAccumulate= oldAccumulate + " - " + absolute_userinput + " = " + accumulatedValue; 
+			
 			
 		}
 		
@@ -197,8 +162,8 @@ public class BusinessAccumulator implements ActionListener {
 		
 		//GRANT - Feel free to change the names of these, I made them called LOGAREA
 		// before you made the GUI 
-		LOGAREA.append(newAccumulate);
-		LOGAREA.setCaretPosition(LOGAREA.getDocument().getLength()); 
+		//logArea.append(newAccumulate);
+		logArea.setCaretPosition(logArea.getDocument().getLength()); 
 	
 	
 	
@@ -213,84 +178,10 @@ public class BusinessAccumulator implements ActionListener {
 	Alden's Error-checking Method
 	
 	Checks for errors and returns false if the value to be accumulated has errors
-	Sets a global variable to what the user inputed as a double*/
+	Sets a global variable to what the user inputed as a double
 	
-	private static boolean CheckForUserInput(String userInput) {
-		boolean userInputContains_d = false;
-		boolean userInputContains_$ = false;
-		// for InputContainsTwoAfterDecimal, 0 indicates no decimal present, 1
-		// indicates decimal present and
-		// satisfies parameters, 2 indicates that there is a decimal present and
-		// it follows parameters
-		int userInputContainsTwoAfterDecimal = 0;
-		boolean userInputContainsDoubleParseError = false;
-		double userValue = 0;
-		String storedUserInput = userInput;
-
-		// Remove excess whitespace and handles a blank input
-		userInput = userInput.trim();
-
-		if (userInput == "") {
-			errorWithUserInput = "";
-			globalUserInputValueMath = 0;
-			return false;
-		}
-
-		// Removes d from the string and sets the boolean value to true
-		if (userInput.endsWith("d")) {
-			if (userInput.length() > 0 && userInput.charAt(userInput.length() - 1) == 'd') {
-				userInput = userInput.substring(0, userInput.length() - 1);
-				userInputContains_d = true;
-			}
-
-		}
-		// removes $ from the string and sets the boolean value to true
-		if (userInput.startsWith("$")) {
-			if (userInput.length() > 0 && userInput.charAt(0) == '$') {
-				userInput = userInput.substring(1, userInput.length());
-				userInputContains_$ = true;
-			}
-
-		}
-
-		if (userInput.contains(".")) {
-			userInputContainsTwoAfterDecimal = 1;
-			int decimalLocation = userInput.indexOf('.');
-			if ((userInput.length() - 3) <= decimalLocation) {
-				userInputContainsTwoAfterDecimal = 2;
-				// //Trace Statements
-				// System.out.println(userInput.length());
-				// System.out.println(decimalLocation);
-				// System.out.println("truee");
-			}
-
-		}
-
-		// Tests the string for everything else
-		try {
-			userValue = Double.parseDouble(userInput);
-		} catch (IllegalArgumentException ex) {
-
-			userInputContainsDoubleParseError = true;
-		}
-
-		globalUserInputValueMath = 0;
-
-		if (userInputContainsDoubleParseError == true) {
-			errorWithUserInput = "amount is not numeric";
-			return false;
-
-		} else if (userInputContainsTwoAfterDecimal == 1) {
-			errorWithUserInput = "Two decimal digits are required following a decimal point.";
-			return false;
-		} else {
-			// CheckUserInput method changes the global variable to equal it's
-			// math form instead of a string
-			globalUserInputValueMath = userValue;
-			return true;
-
-		}
-	/*
+	
+	
 	***********************************************************************************
 	*/
 	
@@ -376,4 +267,5 @@ public class BusinessAccumulator implements ActionListener {
 		}
 
 	}
+
 }
